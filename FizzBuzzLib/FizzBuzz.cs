d75643a29167e4ef792878e05323a8a2cfc25600
cewinn <160144853+cewinn@users.noreply.github.com>
2026-04-27 16:46:12 -0400

    Implement FizzBuzz method in FizzBuzz class

--- FizzBuzzLib/FizzBuzz.cs
+++ FizzBuzzLib/FizzBuzz.cs
@@ -1,12 +1,47 @@
 ﻿namespace FizzBuzzLib
 {
-    public class FizzBuzz //: IFizzBuzz
+     //inherit interface - cw
+    public class FizzBuzz : IFizzBuzz
     {
         public string DemoMethod()
         {
             // Feel free to delete this method
 
             return "Hello World";
+        }
+
+        
+        //implement interface - cw
+        public string FizzBuzzifyAnInt(int input)
+        {
+            try
+            {
+                //test first for both conditions - cw
+                if (input % 3 == 0 && input % 5 == 0)
+                {
+                    return "FizzBuzz";
+                }
+                //test only for divisable by three - cw
+                else if (input % 3 == 0)
+                {
+                    return "Fizz";
+                }
+                //test only for divisable by five - cw
+                else if (input % 5 == 0)
+                {
+                    return "Buzz";
+                }
+                //if tests fail, return int as string
+                else
+                {
+                    return input.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
         }
     }
 }
