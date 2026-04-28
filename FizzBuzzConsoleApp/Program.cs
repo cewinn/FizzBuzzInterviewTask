afaf407a9a810851f13cf2586b909dd271f3b624
cewinn <160144853+cewinn@users.noreply.github.com>
2026-04-27 22:10:41 -0400

    Add test cases for FizzBuzzifyAnInt method
    Added test cases for FizzBuzz functionality with different replacement pairs.

--- FizzBuzzConsoleApp/Program.cs
+++ FizzBuzzConsoleApp/Program.cs
@@ -15,6 +15,23 @@
              {
                  Console.WriteLine(item);
              }
+
+             //test case 1
+         var testCase1 = fizzBuzzer.FizzBuzzifyAnInt(1, 10);
+
+         Console.WriteLine(testCase1);
+
+         //test case 2
+         fizzBuzzer.ReplacementPairs = new Dictionary<int, string> { { 2, "Even" } };
+         var testCase2 = fizzBuzzer.FizzBuzzifyAnInt(1, 10);
+
+         Console.WriteLine(testCase2);
+
+         //test case 2
+         fizzBuzzer.ReplacementPairs = new Dictionary<int, string> { { 2, "Quack" }, { 5, "Honk" } };
+         var testCase3 = fizzBuzzer.FizzBuzzifyAnInt(1, 10);
+        
+         Console.WriteLine(testCase3);
         }
     }
 }
